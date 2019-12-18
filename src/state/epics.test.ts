3e7de9181568345a8da177597cc73c3542c2fb7a
Yann Braga <yannbf@gmail.com>
2019-12-18 16:21:06 +0100

    test(epics): add tests

---
+++ src/state/epics.test.ts
@@ -0,0 +1,55 @@
+import * as actions from './actionTypes'
+import { fetchTasksEpic } from './epics'
+import { database } from '../api/database'
+import { ActionsObservable } from 'redux-observable'
+jest.mock('../api/database')
+
+describe('Task Epics', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('should dispatch success when fetching data', async () => {
+    // Setup
+    ;(database.fetchData as jest.Mock).mockReturnValue(Promise.resolve([]))
+
+    const action$ = ActionsObservable.of({
+      type: actions.FETCH_TASKS,
+    })
+
+    const expectedPayload = {
+      tasks: [],
+      isLoading: false,
+    }
+
+    // Execute
+    const epic$ = fetchTasksEpic(action$)
+    const result = await epic$.toPromise()
+
+    // Assert
+    expect(result.payload).toEqual(expectedPayload)
+    expect(result).toHaveDispatched(actions.FETCH_TASKS_SUCCESS)
+  })
+
+  test('should dispatch failure when fetching data', async () => {
+    // Setup
+    ;(database.fetchData as jest.Mock).mockReturnValue(Promise.reject('error!'))
+
+    const action$ = ActionsObservable.of({
+      type: actions.FETCH_TASKS,
+    })
+
+    // Execute
+    const epic$ = fetchTasksEpic(action$)
+    const result = await epic$.toPromise()
+
+    // Assert
+    const expectedPayload = {
+      error: 'error!',
+      isLoading: false,
+    }
+
+    expect(result.payload).toEqual(expectedPayload)
+    expect(result).toHaveDispatched(actions.FETCH_TASKS_FAILURE)
+  })
+})

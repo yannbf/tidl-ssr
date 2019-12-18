0f87aa990d83503cdb18df4042013070f6f8c3a9
Yann Braga <yannbf@gmail.com>
2019-12-18 16:15:33 +0100

    test(react-testing): add renderWithRedux method
    from documentation
    https://testing-library.com/docs/example-react-redux

---
+++ tests/state.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { render } from '@testing-library/react'
+import reducer, { INITIAL_STATE } from '../src/state/reducers'
+
+export function renderWithRedux(
+  component,
+  { initialState = INITIAL_STATE, store = createStore(reducer, initialState) } = {}
+) {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    // adding `store` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    store,
+  }
+}

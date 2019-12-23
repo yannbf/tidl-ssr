70b322a0e4e81541652064014f5f14f9eaee85b5
Yann Braga <yannbf@gmail.com>
2019-12-23 16:02:40 +0100

    chore: adjust storybook configuration
    - Add date configuration
    - Register icons so fontawesome is available
    - Add tsconfigpaths plugin for relative paths support

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,4 +1,7 @@
 import { configure } from '@storybook/react'
+import { configureDates, registerIcons } from '@ltid/util'
+configureDates()
+registerIcons()
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.tsx$/), module)
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
@@ -7,5 +9,11 @@ module.exports = ({ config, mode }) => {
     },
   })
   config.resolve.extensions.push('.ts', '.tsx')
+
+  if (config.resolve.plugins) {
+    config.resolve.plugins.push(new TsconfigPathsPlugin())
+  } else {
+    config.resolve.plugins = [new TsconfigPathsPlugin()]
+  }
   return config
 }

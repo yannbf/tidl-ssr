adad1b3e08105621b6df01017385f6774569bc9c
Yann Braga <yannbf@gmail.com>
2019-12-26 13:55:24 +0100

    test(task-list): remove redux in testing

--- src/components/TaskList.test.tsx
+++ src/components/TaskList.test.tsx
@@ -1,46 +1,32 @@
 import React from 'react'
-import { cleanup } from '@testing-library/react'
+import { cleanup, render } from '@testing-library/react'
 
-import { renderWithRedux } from '@ltid/tests/state'
 import { TaskList } from './TaskList'
+import { ITask } from '@ltid/types'
 
-// TODO: fix it now that it uses redux
-describe.skip('Task List', () => {
+describe('Task List', () => {
   afterEach(cleanup)
 
   test('Loading element is rendered if no data is there', () => {
-    const { getByTestId } = renderWithRedux(<TaskList />, {
-      initialState: { tasks: [], isLoading: true },
-    })
+    const { getByTestId } = render(<TaskList tasks={null} />)
 
     expect(getByTestId('loading')).toBeInTheDocument()
   })
 
   test('Empty List element is rendered if empty array is passed', async done => {
-    const { container, getByTestId } = renderWithRedux(<TaskList />, {
-      initialState: { tasks: [], isLoading: false },
-    })
-    const loadingElement = getByTestId('loading')
+    const { getByTestId } = render(<TaskList tasks={[]} />)
 
-    // first approach, not working
-    // await waitForElementToBeRemoved(() => loadingElement, { container })
-    // expect(getByTestId('empty-list')).toBeInTheDocument()
-
-    // second approach, not working
-    // const emptyListElement = await waitForElement(() => document.querySelector('[data-testid="empty-list"]'),
-    //   { container }
-    // )
-    // expect(getByTestId('empty-list')).toBeInTheDocument()
+    expect(getByTestId('empty-list')).toBeInTheDocument()
   })
 
   test('All elements of the list are rendered', () => {
     // Setup
-    const data = [
-      { name: 'braga', date: new Date() },
-      { name: 'yann', date: new Date() },
+    const data: ITask[] = [
+      { name: 'braga', date: new Date(), icon: 'tshirt' },
+      { name: 'yann', date: new Date(), icon: 'tshirt' },
     ]
 
-    const { getAllByTestId } = renderWithRedux(<TaskList />)
+    const { getAllByTestId } = render(<TaskList tasks={data} />)
 
     expect(getAllByTestId('list-item').length).toBe(data.length)
   })

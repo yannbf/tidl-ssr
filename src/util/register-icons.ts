25a42ac15c99180e1402d74b9eecc6c327bdc454
Yann Braga <yannbraga@Yanns-MacBook-Pro.local>
2020-01-16 10:59:03 +0100

    feat(icons): add close icon

--- src/util/register-icons.ts
+++ src/util/register-icons.ts
@@ -5,10 +5,11 @@ import {
   faTshirt,
   faUtensils,
   faTrash,
+  faTimes,
 } from '@fortawesome/free-solid-svg-icons'
 
 export const registerIcons = () => {
-  const icons = [faCheckSquare, faCoffee, faTshirt, faUtensils, faTrash]
+  const icons = [faCheckSquare, faCoffee, faTshirt, faUtensils, faTrash, faTimes]
 
   library.add(...icons)
 }

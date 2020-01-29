d203857c39170ae20e18bc531ebefdde6c365761
Yann Braga <yannbraga@Yanns-MacBook-Pro.local>
2020-01-29 19:20:22 +0100

    feat: improve task form UI

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,5 +1,8 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import styled from 'styled-components'
+import Icon from './Icon'
+import dayjs from 'dayjs'
 
 const TaskSchema = Yup.object().shape({
   name: Yup.string()
@@ -11,6 +14,35 @@ const TaskSchema = Yup.object().shape({
     .max(new Date(), 'Do not select dates in the future.')
     .required('Required'),
 })
+
+const FieldSet = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  border: none;
+`
+
+const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+`
+
+const StyledField = styled(Field)`
+  width: 8rem;
+  height: 1.5rem;
+`
+
+const StyledIcon = styled(Icon)`
+  align-self: center;
+`
+
+const SubmitButton = styled.button`
+  width: 6rem;
+  align-self: center;
+  padding: 1rem;
+  font-size: 1rem;
+  background: white;
+  border-radius: 0.75rem;
+`
 
 export const TaskForm = ({ formData, onSubmit }) => {
   const { name = '', date = new Date() } = formData
@@ -28,15 +60,24 @@ export const TaskForm = ({ formData, onSubmit }) => {
       }
     >
       {({ isValid, isSubmitting }) => (
-        <Form>
-          <Field type="text" name="name" />
-          <ErrorMessage name="name" component="div" />
-          <Field type="date" name="date" />
-          <ErrorMessage name="date" component="div" />
-          <button type="submit" disabled={!isValid || isSubmitting}>
+        <StyledForm>
+          <StyledIcon icon="coffee" />
+          <FieldSet>
+            <label htmlFor="name">Name</label>
+            <StyledField type="text" name="name" placeholder="Task name.." />
+            <ErrorMessage name="name" component="div" />
+          </FieldSet>
+
+          <FieldSet>
+            <label htmlFor="name">Last time you did it</label>
+            <StyledField type="date" name="date" max={dayjs().format('YYYY-MM-DD')} />
+            <ErrorMessage name="date" component="div" />
+          </FieldSet>
+
+          <SubmitButton type="submit" disabled={!isValid || isSubmitting}>
             Save
-          </button>
-        </Form>
+          </SubmitButton>
+        </StyledForm>
       )}
     </Formik>
   )

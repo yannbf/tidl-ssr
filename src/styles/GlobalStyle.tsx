d3b9a70fbdc944fbc8443651aeda775f8d1ff623
Yann Braga <yannbraga@Yanns-MacBook-Pro.local>
2020-01-29 19:11:25 +0100

    style: add Open Sans as global font

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap');
   body {
+    font-family: 'Open Sans', sans-serif;
     background: ${({ theme }) => theme.bg.primary};
     font-size: 1rem;
   }

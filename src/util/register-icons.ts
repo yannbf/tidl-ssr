3e8bec5f36a85d0758ec3d7a798ac20ffe160b62
Yann Braga <yannbf@gmail.com>
2020-02-05 12:15:19 +0100

    feat: register only a set of icons
    - reduces bundle size
    - personalizes UX

--- src/util/register-icons.ts
+++ src/util/register-icons.ts
@@ -1,9 +1,11 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import * as Icons from '@fortawesome/free-solid-svg-icons'
+import { Icons } from './icons'
 
 export const iconList = Object.keys(Icons)
   .filter(key => key !== 'fas' && key !== 'prefix')
-  .map(icon => Icons[icon])
+  // necessary in order to use normal icons + brand icons together
+  // changes prefix of brand icons from fab to fas, which is used by default
+  .map(icon => ({ ...Icons[icon], prefix: 'fas' }))
 
 export const registerIcons = () => {
   library.add(...iconList)

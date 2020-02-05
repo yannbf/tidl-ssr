87afb4b15a81e0f673712baf1cfdb9c3c84e25c2
Yann Braga <yannbf@gmail.com>
2020-02-05 11:02:58 +0100

    chore: add language to html tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="manifest.json" />

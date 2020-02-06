9e8ce7871c88b1c21805565223bcb990928544d1
Yann Braga <yannbf@gmail.com>
2020-02-06 16:57:03 +0100

    refactor(breakpoints): remove unused object

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,24 +1,9 @@
-import { css, CSSObject } from 'styled-components'
-
 const sizes = {
   giant: 1170,
   desktop: 992,
   tablet: 768,
   phone: 376,
 }
-
-// iterate through the sizes and create a media template
-export const mediaBreakpoint: any = Object.keys(sizes).reduce((accumulator, label) => {
-  // use em in breakpoints to work properly cross-browser and support users
-  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
-  const emSize = sizes[label] / 16
-  accumulator[label] = (cssArgs: CSSObject) => css`
-    @media (min-width: ${emSize}em) {
-      ${css(cssArgs)};
-    }
-  `
-  return accumulator
-}, {})
 
 export const device: any = Object.keys(sizes).reduce((accumulator, label) => {
   // use em in breakpoints to work properly cross-browser and support users

32d242092aebe77d3703b9dce919098e6f51c094
Yann Braga <yannbf@gmail.com>
2020-02-06 10:06:18 +0100

    feat(analytics): log pageview on page template

--- src/components/PageTemplate.tsx
+++ src/components/PageTemplate.tsx
@@ -1,5 +1,10 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import styled from 'styled-components'
+
+import { initGA, logPageView } from '@ltid/util'
+
+declare const window
 
 type Props = {
   title?: string
@@ -10,11 +15,21 @@ const Wrapper = styled.section`
   padding: 1rem;
 `
 
-export const PageTemplate: React.FC<Props> = ({ title, children }) => (
-  <>
-    <Head>
-      <title>Tidl{title && ` - ${title}`}</title>
-    </Head>
-    <Wrapper>{children}</Wrapper>
-  </>
-)
+export const PageTemplate: React.FC<Props> = ({ title, children }) => {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Tidl{title && ` - ${title}`}</title>
+      </Head>
+      <Wrapper>{children}</Wrapper>
+    </>
+  )
+}

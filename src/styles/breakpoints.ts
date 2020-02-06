9fd3798b96f818544c318e050125256e4ef02079
Yann Braga <yannbf@gmail.com>
2020-02-06 16:42:53 +0100

    style: add device breakpoint
    - it works better than mediaBreakpoint for code highlighting.

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -19,3 +19,11 @@ export const mediaBreakpoint: any = Object.keys(sizes).reduce((accumulator, labe
   `
   return accumulator
 }, {})
+
+export const device: any = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+  const emSize = sizes[label] / 16
+  accumulator[label] = `(min-width: ${emSize}em)`
+  return accumulator
+}, {})

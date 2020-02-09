6083e8b9795069fbc29cef96149233a479e55fc4
Yann Braga <yannbf@gmail.com>
2020-02-09 13:01:39 +0100

    chore(mocks): fix  data after updating types

--- src/api/mock-data.ts
+++ src/api/mock-data.ts
@@ -6,17 +6,20 @@ export const mockData: ITask[] = [
     name: 'Laundry',
     date: new Date().toString(),
     icon: 'tshirt',
+    frequency: 'none',
   },
   {
     id: 2,
     name: 'Take trash out',
     date: new Date(Date.now() + 1000 * 60 * 60 * 248).toString(),
     icon: 'trash',
+    frequency: 'none',
   },
   {
     id: 3,
     name: 'Batch cooking',
     date: new Date(Date.now() + 1000 * 60 * 60 * 24).toString(),
     icon: 'utensils',
+    frequency: 'none',
   },
 ]

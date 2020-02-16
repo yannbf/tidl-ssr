eaf76e13ca99cd1fa34d4a2c4f1bd59bd41dad60
Yann Braga <yannbf@gmail.com>
2020-02-16 09:28:47 +0100

    feat(component): add list-error

---
+++ src/components/ListError/index.tsx
@@ -0,0 +1,19 @@
+import { Text } from '@tidl/components'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+type Props = {
+  onRetry: () => void
+}
+
+export const ListError: React.FC<Props> = ({ onRetry }) => (
+  <Wrapper>
+    <Text fontWeight="bold">Oops! There was an error.</Text>
+    <button onClick={onRetry}>Try again</button>
+  </Wrapper>
+)

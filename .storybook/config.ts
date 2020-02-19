e69d1654eacfd53d040bfa628d0dcf1fc2ad6f92
Yann Braga <yannbf@gmail.com>
2020-02-19 10:26:36 +0100

    chore(storybook): add a11y check on all stories

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,7 +1,7 @@
 import { addParameters, configure, addDecorator } from '@storybook/react'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { withThemesProvider } from 'storybook-addon-styled-component-theme'
-
+import { withA11y } from '@storybook/addon-a11y'
 import { withRedux } from './decorators'
 import { configureDates, registerIcons } from '@tidl/util'
 import { darkTheme, lightTheme } from '@tidl/styles'
@@ -18,6 +18,7 @@ addParameters({
 
 addDecorator(withRedux)
 addDecorator(withThemesProvider([lightTheme, darkTheme]))
+addDecorator(withA11y)
 
 // automatically import all files ending in *.stories.tsx
 configure(require.context('../src/components', true, /\.stories\.tsx$/), module)

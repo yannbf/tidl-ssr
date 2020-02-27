29eef02cb4a68f2d4b431eb84815910b2c711ba3
Yann Braga <yannbf@gmail.com>
2020-02-27 15:28:06 +0100

    fix(task-list-container): wait for user id

--- src/components/TaskListContainer/index.tsx
+++ src/components/TaskListContainer/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import {
   useFirestoreConnect,
@@ -14,34 +13,38 @@ import { ITask } from '@tidl/types'
 export const TaskListContainer: React.FC = () => {
   const userId = useSelector(({ firebase }) => firebase.auth.uid)
 
-  /* Every task has the ownerID in the sharedWith property.
-   * asks with more than one element in that ID means that the task is shared.
-   * This is an efficient way to be able to retrieve all tasks in a single operation. */
-  const tasksQuery: ReduxFirestoreQuerySetting = {
-    collection: 'tasks',
-    where: ['sharedWith', 'array-contains', userId],
+  if (isLoaded(userId)) {
+    /* Every task has the ownerID in the sharedWith property.
+     * asks with more than one element in that ID means that the task is shared.
+     * This is an efficient way to be able to retrieve all tasks in a single operation. */
+    const tasksQuery: ReduxFirestoreQuerySetting = {
+      collection: 'tasks',
+      where: ['sharedWith', 'array-contains', userId],
+    }
+
+    useFirestoreConnect(() => [tasksQuery])
+
+    const tasks = useSelector(({ firestore }) => {
+      return firestore.data.tasks
+    })
+
+    if (!isLoaded(tasks)) {
+      return (
+        <div data-testid="loading">
+          <Text>loading..</Text>
+        </div>
+      )
+    }
+
+    if (isEmpty(tasks)) {
+      return <EmptyList />
+    }
+
+    // This filter is necessary to handle delete operations
+    const tasksArray = Object.values(tasks).filter(Boolean) as ITask[]
+
+    return <TaskList tasks={tasksArray} />
   }
 
-  useFirestoreConnect(() => [tasksQuery])
-
-  const tasks = useSelector(({ firestore }) => {
-    return firestore.data.tasks
-  })
-
-  if (!isLoaded(tasks)) {
-    return (
-      <div data-testid="loading">
-        <Text>loading..</Text>
-      </div>
-    )
-  }
-
-  if (isEmpty(tasks)) {
-    return <EmptyList />
-  }
-
-  // This filter is necessary to handle delete operations
-  const tasksArray = Object.values(tasks).filter(Boolean) as ITask[]
-
-  return <TaskList tasks={tasksArray} />
+  return <Text>loading..</Text>
 }
